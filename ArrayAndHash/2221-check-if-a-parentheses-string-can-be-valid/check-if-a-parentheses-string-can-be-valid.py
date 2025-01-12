fd4a9935aa2ceb4a412f0e9dbe442425232b5a26
Abhishek Shankar <abhishekshankar2708@gmail.com>
2025-01-12 18:59:33 +0530

    Time: 183 ms (13.83%) | Memory: 18.6 MB (57.06%) - LeetSync

--- ArrayAndHash/2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ ArrayAndHash/2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -3,13 +3,13 @@ class Solution:
         n=len(s)
         if n&1==1: return False
         
-        bMin, bMax=0, 0  # Initialize balance
+        bMin, bMax=0, 0 
         
         for i, c in enumerate(s):
             op=c== '('
             wild=locked[i]=='0'
             
-            # Update balances
+        
             if (not op) or wild: bMin-=1  
             else: bMin+=1  
             
@@ -20,7 +20,7 @@ class Solution:
             
             bMin = max(bMin, 0)
         
-        # Check if the parentheses can be 
+        
         return bMin==0
 
         
